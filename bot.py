cfdee456a2235104ea10e044db9713ca2f8231ee
Alexey <chuff184@gmail.com>
2018-12-20 14:50:38 +0300

    Add error handling

--- bot.py
+++ bot.py
@@ -4,13 +4,13 @@ import os
 import requests
 
 #here put path to your credentials file(.json)
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="YOUR_CREDENTIALS_HERE!"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="google_cloud.json"
 
 #creating vision client
 client=vision.ImageAnnotatorClient()
 
 #paste your bot token here
-token="TOKEN_HERE!"
+token="578032939:AAGhpa95-6k9q63EuVfmZHUUk0O7Ab3b2EY"
 
 bot=telebot.AsyncTeleBot(token)
 
@@ -20,12 +20,15 @@ def photo(msg):
     #download file
     file_info=bot.get_file(msg.photo[-1].file_id).wait()
     img=bot.download_file(file_info.file_path).wait()
-    #get text from image
-    text=client.annotate_image({
+    #get text from image, if error occured-stop
+    try:
+      text=client.annotate_image({
   "image": {"content":img},
   "features": [{"type": vision.enums.Feature.Type.TEXT_DETECTION}],
   "image_context":{"language_hints":["en"]}
 }).full_text_annotation.text
+    except Exception:
+      return
     #send the text to http://0x0.st and send user the link
     hasted_url=requests.post("http://0x0.st",files={"file":text}).text.rstrip()
     bot.send_message(msg.chat.id,"*Here it is the information I could get from the picture:*\n"+hasted_url,parse_mode="markdown")
